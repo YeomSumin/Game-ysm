b552577b640ee84aa8fa198d03c03f448cb775eb
YeomSumin <dbrtlqcho60@naver.com>
2016-11-15 22:29:17 +0900

    Project

--- 2D_Project/main_state.py
+++ 2D_Project/main_state.py
@@ -9,6 +9,9 @@ import title_state
 name = "MainState"
 
 current_time = 0.0
+change = False
+moment = False
+pick = False
 back = None
 stem = None
 head = None
@@ -58,6 +61,8 @@ class character:
         self.state = self.STILL
         self.chframe, self.chreframe, self.count = 0, 0, 0
         self.total_frames, self.total_rframes = 0.0, 0.0
+        self.level = 0.5
+        self.stage = False
         self.x, self.y = 275, 600
         self.chimage = load_image('character.png')
         self.chreimage = load_image('character_resist.png')
@@ -83,18 +88,18 @@ class character:
 
 
     def update(self, frame_time):
-        distance = character.RUN_SPEED_PPS * frame_time
+        self.distance = character.RUN_SPEED_PPS * frame_time
         self.total_frames += character.FRAMES_PER_ACTION * character.ACTION_PER_TIME * frame_time
         self.total_rframes += character.FRAMES_PER_RACTION * character.RACTION_PER_TIME * frame_time
         self.count += 1
         self.y -= 0.01
 
         if self.state == self.UP:
-            self.y += distance
+            self.y += self.distance
         if self.state == self.LEFT:
-            self.x -= distance
+            self.x -= self.distance
         if self.state == self.RIGHT:
-            self.x += distance
+            self.x += self.distance
 
         if self.count % 100 == 0:
             #self.chframe = (self.chframe + 1) % 10
@@ -103,7 +108,7 @@ class character:
 
         if self.absorb:
             if self.y > 140:
-                self.y -= 0.5
+                self.y -= self.level
 
     def draw(self):
         if self.absorb == False:
@@ -111,35 +116,69 @@ class character:
         else:
             self.chreimage.clip_draw(self.chreframe * 85, 0, 85, 160, self.x, self.y, 60, 87) #63 90
 
+    def get_bb(self):
+        return self.x - 3, self.y - 3, self.x + 3, self.y + 3 #28 43
 
 class bomb:
+    TIME_PER_ACTION = 4
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 10
+
     boimage = None
     exploimage = None
 
     def __init__(self):
         self.absorb = False
         self.spit = False
+        self.turn = False
+        self.coll = False
+        self.put = False
+        self.explosion = False
         self.suck = random.randint(1, 7)
+        self.move = 0
         self.x = 275
         self.y = 600
+        self.total_frames = 0
         self.boframe = random.randint(0, 10)
+        self.exframe = 0
         self.count = random.randint(0, 9)
         if bomb.boimage == None:
             bomb.boimage = load_image('bomb.png')
         if bomb.exploimage == None:
             bomb.exploimage = load_image('explosion.png')
 
-    def update(self):
+    def handle_event(self, event):
+        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_z):
+            self.coll = False
+            self.put = True
+
+    def update(self, frame_time):
+        self.total_frames += bomb.FRAMES_PER_ACTION * bomb.ACTION_PER_TIME * frame_time
         self.count += 1
 
-        if self.count % 10 == 0:
-            self.boframe = (self.boframe + 1) % 10
+        if self.count % 100 == 0:
+            self.boframe = int(self.total_frames + 1) % 10
+
+        if self.put:
+            if self.count % 10 == 0:
+                if self.exframe == 4:
+                    self.explosion = False
+                self.exframe = (self.exframe + 1) % 5
 
         if self.absorb:
             if self.spit:
-                if self.y < 150:
-                    self.y += 0.9
+                if (self.suck % 3) == 0:
+                    if self.coll == False:
+                        self.y += 1
+                        if (self.move % 2) == 0:
+                            self.x += 3/10
+                        else:
+                            self.x -= 3/10
+
+                        #self.turn = False
             else:
+                self.turn = True
+                self.move = random.randint(1, 5)
                 if self.boframe < 10:
                     self.boframe = random.randint(10, 11)
                 if self.count % 10 == 0:
@@ -148,19 +187,28 @@ class bomb:
                     else:
                         self.boframe = (self.boframe + 1) % 13
 
-                if (self.suck % 3) or (self.suck % 7) == 0:
-                    if self.y > 140:
+                if (self.suck % 3) == 0 and (self.coll == False):
+                    if self.y < 720 and self.y > 140:
                         self.y -= 0.9
 
                     if self.x < 280:
                         self.x += 0.7
                     else:
                         self.x -= 0.7
+
         else:
-            self.suck = random.randint(0, 7)
+            if self.turn == False and self.coll == False:
+                self.suck = random.randint(0, 7)
+
 
     def draw(self):
-        self.boimage.clip_draw(self.boframe * 50, 0, 50, 60, self.x, self.y, 35, 45)
+        if self.explosion == True:
+            self.exploimage.clip_draw(self.exframe * 120, 0, 120, 100, self.x, self.y, 60, 50)
+        else:
+            self.boimage.clip_draw(self.boframe * 50, 0, 50, 60, self.x, self.y, 35, 45)
+
+    def get_bb(self):
+        return self.x - 18, self.y - 28, self.x + 18, self.y + 28
 
 
 def create_bombgroup():
@@ -180,6 +228,18 @@ def create_bombgroup():
     return bombgroup
 
 
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+
 class flower_leg:
     def __init__(self):
         self.count = 0
@@ -194,13 +254,13 @@ class flower_leg:
     def update(self):
         self.count += 1
 
-        if self.count % 11 == 0:
+        if self.count % 110 == 0:
             self.legf1 = (self.legf1 + 1) % 2
 
-        if self.count % 17 == 0:
+        if self.count % 170 == 0:
             self.legf3 = (self.legf3 + 1) % 2
 
-        if self.count % 23 == 0:
+        if self.count % 230 == 0:
             self.legf4 = (self.legf4 + 1) % 3
 
     def draw(self):
@@ -211,30 +271,40 @@ class flower_leg:
 
 
 class flower_head:
+    TIME_PER_ACTION = 4
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 2
+
     def __init__(self):
         self.absorb = False
+        self.x = 280; self.y = 160
         self.frame = 0
+        self.total_frames = 0
         self.abframe = 2
         self.count = 0
         self.image = load_image('flower_head.png')
 
-    def update(self):
+    def update(self, frame_time):
+        self.total_frames += flower_head.FRAMES_PER_ACTION * flower_head.ACTION_PER_TIME * frame_time
         self.count += 1
 
         if self.absorb:
-            if self.count % 23 == 0:
+            if self.count % 230 == 0:
                 self.abframe += 1
                 if self.abframe == 4:
                     self.abframe = 2
         else:
             if self.count % 25 == 0:
-                self.frame = (self.frame + 1) % 2
+                self.frame = int(self.total_frames + 1) % 2
 
     def draw(self):
         if self.absorb:
             self.image.clip_draw(self.abframe * 400, 0, 400, 400, 280, 160, 240, 240)
         else:
             self.image.clip_draw(self.frame * 400, 0, 400, 400, 280, 160, 240, 240)
+
+    def get_bb(self):
+        return self.x - 130, self.y - 110, self.x + 130, self.y + 110
 
 def get_frame_time():
     global current_time
@@ -270,6 +340,7 @@ def resume():
     pass
 
 def handle_events():
+    global change, moment, pick
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -278,19 +349,52 @@ def handle_events():
             game_framework.change_state(title_state)
         else:
             mario.handle_event(event)
+            for bombs in seeds:
+                bombs.handle_event(event)
 
     if back.absorb:
-        #head.absorb = True
-        mario.absorb = True
-        for bombs in seeds:
-            bombs.absorb = True
-    else:
-        #head.absorb = False
-        mario.absorb = False
-        for bombs in seeds:
-            bombs.absorb = False
-            bombs.spit = False
+        if change:
+            head.absorb = True
+            for bombs in seeds:
+                bombs.absorb = True
+                bombs.spit = True
+                if collide(mario, bombs):
+                    bombs.coll = True
+                    bombs.x = mario.x
+                    bombs.y = mario.y - 10
 
+            moment = False
+        else:
+            for bombs in seeds:
+                if bombs.coll and bombs.put == False:
+                    bombs.x = mario.x
+                    bombs.y = mario.y - 10
+                if collide(head, bombs) and bombs.put:
+                    bombs.explosion = True
+                    mario.stage = True
+            head.absorb = True
+            mario.absorb = True
+            for bombs in seeds:
+                bombs.absorb = True
+            moment = True
+    else:
+        if moment:
+            change = True
+            mario.absorb = False
+            head.absorb = False
+            for bombs in seeds:
+                bombs.absorb = False
+        else:
+            for bombs in seeds:
+                if bombs.coll:
+                    bombs.x = mario.x
+                    bombs.y = mario.y - 10
+            head.absorb = False
+            mario.absorb = False
+            for bombs in seeds:
+                bombs.absorb = False
+                bombs.spit = False
+            change = False
 
 def update():
     global frame_time
@@ -300,8 +404,8 @@ def update():
     mario.update(frame_time)
     stem.update()
     for bombs in seeds:
-        bombs.update()
-    head.update()
+        bombs.update(frame_time)
+    head.update(frame_time)
 
 
 def draw():
@@ -310,6 +414,10 @@ def draw():
     mario.draw()
     stem.draw()
     for bombs in seeds:
-        bombs.draw()
-    head.draw()
+        if bombs.put:
+            head.draw()
+            bombs.draw()
+        else:
+            bombs.draw()
+            head.draw()
     update_canvas()
